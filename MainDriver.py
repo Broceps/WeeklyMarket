55c3441f81857eb5abd09437e9692d835d1c1443
Broceps <robhinio96@hotmail.com>
2020-06-30 21:29:21 +0200

    added main driver

---
+++ MainDriver.py
@@ -0,0 +1,6 @@
+import NewsAPIController
+import StockAPIController
+
+class MainDriver:
+    _news_controller = NewsAPIController()
+    _stock_controller = StockAPIController()
--- NewsAPIController.py
+++ NewsAPIController.py
@@ -4,10 +4,10 @@ class NewsAPIController:
     _api_Key = open("files/news_api_key.txt").read()
 
     def fetch_news(self, start, end, company):
-        week_start_date = start
-        week_end_date = end
-        keyword = company
-        url = "https://newsapi.org/v2/everything?q=" + keyword +"&from=" + week_start_date + "&to=" + week_end_date + "&sortBy=popularity&apiKey=" + self._api_Key
+        start = start
+        end = end
+        company = company
+        url = "https://newsapi.org/v2/everything?q=" + company +"&from=" + start + "&to=" + end + "&sortBy=popularity&apiKey=" + self._api_Key
 
         req = requests.get(url)
         result = req.json()
@@ -28,5 +28,5 @@ week_start_date = "2020-06-22"
 week_end_date = "2020-06-26"
 keyword = "NVDA"
 
-c = NewsAPIController
-print(c.fetch_news(c, week_start_date, week_end_date, keyword).get("article"))
+c = NewsAPIController()
+c.fetch_news(week_start_date, week_end_date ,keyword)
