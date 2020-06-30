020bc755cf5cb9f84ba2335a5f26de9c664126ca
Broceps <robhinio96@hotmail.com>
2020-06-30 22:09:08 +0200

    updated main class

--- main/MainDriver.py
+++ main/MainDriver.py
@@ -4,3 +4,14 @@ from controller import NewsAPIController, StockAPIController
 class MainDriver:
     _news_controller = NewsAPIController()
     _stock_controller = StockAPIController()
+
+    _newsletter_msg = "Dear {0}, here is your weekly newsletter!\n" \
+                      "--------\n{1}--------\n" \
+                      "lowest price: ${2}, highest price: ${3}, closing price: {4}\n" \
+                      "{1} was mentioned in the following articles this week:\n" \
+                      "{5}"
+
+    def print_newsletter(self, name, company):
+        stock_data = StockAPIController
+
+        print()
