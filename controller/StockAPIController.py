12f0fcc03f7c8175fd4878adfdbe7d76e749921f
MartinDJEriksson <martin.eriksson7@outlook.com>
2020-06-30 22:01:23 +0200

    Updated def daily prices

--- controller/StockAPIController.py
+++ controller/StockAPIController.py
@@ -1,38 +1,60 @@
 import requests
 
 stock_api_key = open('_files/stock_api_key.txt').read()
-stock = "FB"
 
 # companies = ["NVDA", "AAPL", "FB", "NFLX", "MFST", "GOOGL", "WMT", "JPM"]
 
 class StockAPIController():
 
-    def daily_prices():
+
+    def daily_prices(self):
         date = "2020-06-26"
+        stock = "FB"
         interval_daily = "DAILY"
         high = "2. high"
         low = "3. low"
         close = "4. close"
         url = "https://www.alphavantage.co/query?function=TIME_SERIES_" + interval_daily + "&symbol=" + stock + "&apikey=" + stock_api_key
         r = requests.get(url)
-        print(round(float((r.json()["Time Series (Daily)"][date][high])), 2))
-        print(round(float((r.json()["Time Series (Daily)"][date][low])), 2))
-        print(round(float((r.json()["Time Series (Daily)"][date][close])), 2))
-    daily_prices()
 
-    def weekly_prices():
-        date = "2020-06-26"
-        interval_weekly = "WEEKLY"
-        high = "2. high"
-        low = "3. low"
-        close = "4. close"
-        url = "https://www.alphavantage.co/query?function=TIME_SERIES_" + interval_weekly + "&symbol=" + stock + "&apikey=" + stock_api_key
-        r = requests.get(url)
-        print(round(float((r.json()["Weekly Time Series"][date][high])), 2))
-        print(round(float((r.json()["Weekly Time Series"][date][low])), 2))
-        print(round(float((r.json()["Weekly Time Series"][date][close])), 2))
-    weekly_prices()
-StockAPIController()
+        high_price = round(float((r.json()["Time Series (Daily)"][date][high])), 2)
+        low_price = round(float((r.json()["Time Series (Daily)"][date][low])), 2)
+        close_price = round(float((r.json()["Time Series (Daily)"][date][close])), 2)
+
+        result = {"Company": stock, "Highest Price ": high_price, "Lowest Price: ": low_price, "Close Price: ": close_price}
+        return result
+        # print(round(float((r.json()["Time Series (Daily)"][date][high])), 2))
+        # print(round(float((r.json()["Time Series (Daily)"][date][low])), 2))
+        # print(round(float((r.json()["Time Series (Daily)"][date][close])), 2))
+
+
+    # def weekly_prices(self):
+    #     date = "2020-06-26"
+    #     stock = "FB"
+    #     interval_weekly = "WEEKLY"
+    #     high = "2. high"
+    #     low = "3. low"
+    #     close = "4. close"
+    #     url = "https://www.alphavantage.co/query?function=TIME_SERIES_" + interval_weekly + "&symbol=" + stock + "&apikey=" + stock_api_key
+    #     r = requests.get(url)
+    #
+    #     # high_price =
+    #     # low_price =
+    #     # close_price =
+    #
+    #     print(round(float((r.json()["Weekly Time Series"][date][high])), 2))
+    #     print(round(float((r.json()["Weekly Time Series"][date][low])), 2))
+    #     print(round(float((r.json()["Weekly Time Series"][date][close])), 2))
+    # weekly_prices()
+
+# StockAPIController()
+
+
+
+
+
+
+
 
 
 'DAILY'
