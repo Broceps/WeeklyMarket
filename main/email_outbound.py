123f3e73f74bcdbd9f5e7614183ae48e336a21bf
Broceps <robhinio96@hotmail.com>
2020-07-03 22:02:50 +0200

    email working

--- main/email_outbound.py
+++ main/email_outbound.py
@@ -1,8 +1,8 @@
 import smtplib
 
-server = smtplib.SMTP_SSL("smtp.gmail.com,", 465)
-server.login("WeeklyMarketNews@gmail.com", "WeeklyMarket2020")
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+server.login("weeklymarketnews@gmail.com", "WeeklyMarket2020")
 server.sendmail("WeeklyMarketNews@gmail.com",
-                "martin.eriksson7@outlook.com",
+                "m.r.newsletter.demo@gmail.com",
                 "Do you see me...?")
 server.quit()
