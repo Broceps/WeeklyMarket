d80d59a27030407108bba47819c8d0039c36a24e
MartinDJEriksson <martin.eriksson7@outlook.com>
2020-07-03 23:19:59 +0200

    EmailController.py Subject Title added

--- controller/EmailController.py
+++ controller/EmailController.py
@@ -1,19 +1,35 @@
 import smtplib
+from email.message import EmailMessage
+
 
 
 class EmailController:
     email_un = open("../_files/email_un.txt").read()
     email_pw = open("../_files/email_pw.txt").read()
+    email_recipient = "m.r.newsletter.demo@gmail.com"
 
+    def email_sendout(self):
+        msg = """Subject: One More Time! \n
+Ahri must go on!"""
 
-    def email_sendout(self, email_recipient, email_msg):
         server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
         server.login(self.email_un, self.email_pw)
-        server.sendmail(self.email_un, email_recipient, email_msg)
+        server.sendmail(self.email_un, [self.email_recipient], msg)
         server.quit()
 
-email_recipient = "m.r.newsletter.demo@gmail.com"
-email_msg = "2"
 
 c = EmailController()
-c.email_sendout(email_recipient, email_msg)
+c.email_sendout()
+
+
+
+
+
+
+
+
+
+
+
+
+
