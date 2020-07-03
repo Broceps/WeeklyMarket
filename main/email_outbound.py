af343cdb72d0b8608eab3ea1b4c7c3ef2e2c5124
MartinDJEriksson <martin.eriksson7@outlook.com>
2020-07-03 22:32:10 +0200

    email_outbound.py added

--- main/email_outbound.py
+++ main/email_outbound.py
@@ -1,8 +1,15 @@
 import smtplib
 
-server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-server.login("weeklymarketnews@gmail.com", "WeeklyMarket2020")
-server.sendmail("WeeklyMarketNews@gmail.com",
-                "m.r.newsletter.demo@gmail.com",
-                "Do you see me...?")
-server.quit()
+
+class EmailController:
+    email_un = open("../_files/email_un.txt").read()
+    email_pw = open("../_files/email_pw.txt").read()
+    email_sender = "WeeklyMarketNews@gmail.com"
+    email_recipient = "m.r.newsletter.demo@gmail.com"
+    email_msg = "1"
+
+    def email_sendout(self):
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.login(email_un, email_pw)
+        server.sendmail(email_sender, email_recipient, email_msg)
+        server.quit()
